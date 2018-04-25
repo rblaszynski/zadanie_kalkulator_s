dce587112ad571e85da5da72edcb8bd73930bb6e
Robert <32738813+rblaszynski@users.noreply.github.com>
2018-04-25 20:03:00 +0200

    Initial commit

---
+++ src/main/java/com/robert/calculator/ZadanieKalkulatorSApplication.java
@@ -0,0 +1,12 @@
+package com.robert.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieKalkulatorSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadanieKalkulatorSApplication.class, args);
+    }
+}
